d04eaff864c17d5717701b2eacf55da5db89dd64
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-01-05 14:29:56 +0100

    upgrade for dict, import

---
+++ install_newest_vscode_extension.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "IF ERROR:"
+echo "vscode has to be enabled as command code. Can be done from vscode"
+
+code --install-extension vscode-extension/beta_changes/lsp-incc-vscode.vsix
+
+echo "Open vscode and create/ open .incc file or .incc24 file"
+echo ""
+echo ""
+echo "IF ERROR:"
+echo "USE python>=3.12"
+echo "numpy has to be installed for python, bundle packaging not working with numpy (it also has 20 mb...)"
+echo "todo: python -m pip install numpy"
+echo ""
+
+
